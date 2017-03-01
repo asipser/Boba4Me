2e0f930e033fd0fddeab6a61959ccb169c78216e
Tricia Shi <tricia.shi@gmail.com>
2017-02-28 23:45:42 -0500

    fixed the pricing

--- client/stores.js
+++ client/stores.js
@@ -30,78 +30,71 @@ export const STORES = {
 					{
 						"name" : "KF Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "KF Milk Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "KF Oolong Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "Taro Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75},
-							{"name" : "Hot", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23},
+							{"name" : "Hot", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Taro Milk Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75},
-							{"name" : "Hot", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23},
+							{"name" : "Hot", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Coconut Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75},
-							{"name" : "Hot", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23},
+							{"name" : "Hot", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Coffee Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75},
-							{"name" : "Hot", "price" : 4.75}
-						]
-					},
-					{
-						"name" : "Oreo Milk Tea",
-						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23},
+							{"name" : "Hot", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Almond Milk Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Winter Melon Milk Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.23}
 						]
 					}
 					
@@ -113,81 +106,73 @@ export const STORES = {
 					{
 						"name" : "KF Black Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.00},
-							{"name" : "Large", "price" : 3.75},
-							{"name" : "Hot", "price" : 3.75}
+							{"name" : "Medium", "price" : 3.30},
+							{"name" : "Large", "price" : 4.13},
+							{"name" : "Hot", "price" : 4.13}
 						]
 					},
 					{
 						"name" : "KF Green Tea", 
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.00},
-							{"name" : "Large", "price" : 3.75},
-							{"name" : "Hot", "price" : 3.75}
-						]
-					},
-					{
-						"name" : "KF Oolong Tea",
-						"sizes" : [
-							{"name" : "Medium", "price" : 3.00},
-							{"name" : "Large", "price" : 3.75},
-							{"name" : "Hot", "price" : 3.75}
+							{"name" : "Medium", "price" : 3.30},
+							{"name" : "Large", "price" : 4.13},
+							{"name" : "Hot", "price" : 4.13}
 						]
 					},
 					{
 						"name" : "KF Honey Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.00},
-							{"name" : "Large", "price" : 3.75},
-							{"name" : "Hot", "price" : 3.75}
+							{"name" : "Medium", "price" : 3.30},
+							{"name" : "Large", "price" : 4.13},
+							{"name" : "Hot", "price" : 4.13}
 						]
 					},
 					{
 						"name" : "Winter Melon Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.00},
-							{"name" : "Large", "price" : 3.75},
-							{"name" : "Hot", "price" : 3.75}
+							{"name" : "Medium", "price" : 3.30},
+							{"name" : "Large", "price" : 4.13},
+							{"name" : "Hot", "price" : 4.13}
 						]
 					},
 					{
 						"name" : "KF Pomelo Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "Honey Black Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "Honey Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "Longan Jujube Date Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					},
 					{
 						"name" : "Winter Melon Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.50},
-							{"name" : "Hot", "price" : 4.50}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.95},
+							{"name" : "Hot", "price" : 4.95}
 						]
 					}
 					
@@ -199,85 +184,85 @@ export const STORES = {
 					{
 						"name" : "Honey Lemonade",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.25}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.68}
 						]
 					},
 					{
 						"name" : "Lemon Black Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.25}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.68}
 						]
 					},
 					{
 						"name" : "Lemon Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.25},
-							{"name" : "Large", "price" : 4.25}
+							{"name" : "Medium", "price" : 3.58},
+							{"name" : "Large", "price" : 4.68}
 						]
 					},
 					{
 						"name" : "Lychee Punch",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Mango Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Peach Oolong Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Orange Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Veryberry Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Grapefruit Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Sunshine Pineapple Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Passion Fruit Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					},
 					{
 						"name" : "Strawberry Lemon Green Tea",
 						"sizes" : [
-							{"name" : "Medium", "price" : 3.75},
-							{"name" : "Large", "price" : 4.75}
+							{"name" : "Medium", "price" : 4.13},
+							{"name" : "Large", "price" : 5.23}
 						]
 					}
 				]
@@ -287,85 +272,85 @@ export const STORES = {
 					{
 						"name" : "Red Bean Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Matcha Red Bean Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Taro Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Mung Bean Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Mango Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Pineapple Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Pina Colada Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Passion Fruit Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Strawberry Milk Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Coffee Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Italian Mocha Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Caramel Macchiato Slush",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.25},
-							{"name" : "Large", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.68},
+							{"name" : "Large", "price" : 5.50}
 						]
 					}
 			  ]
@@ -375,33 +360,33 @@ export const STORES = {
 					{
 						"name" : "Regular Wow Milk",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 5.00},
-							{"name" : "Hot", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.50},
+							{"name" : "Hot", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Red Bean Wow Milk",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 5.00},
-							{"name" : "Hot", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.50},
+							{"name" : "Hot", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Mung Bean Wow Milk",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 5.00},
-							{"name" : "Hot", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.50},
+							{"name" : "Hot", "price" : 5.50}
 						]
 					},
 					{
 						"name" : "Herbal Jelly Wow Milk",
 						"sizes" : [
-							{"name" : "Medium", "price" : 4.00},
-							{"name" : "Large", "price" : 5.00},
-							{"name" : "Hot", "price" : 5.00}
+							{"name" : "Medium", "price" : 4.40},
+							{"name" : "Large", "price" : 5.50},
+							{"name" : "Hot", "price" : 5.50}
 						]
 					}
 			  ]
